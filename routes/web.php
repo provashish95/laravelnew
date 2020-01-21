fbd896da003175bbd6b263859bf28083beca4254
provashish95 <roytonmoy95@gmail.com>
2020-01-22 00:54:17 +0600

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('tonmoy')->group(function (){
+Route::get('home', function (){
+   echo "this is home";
+});
+
     Route::get('/about', function (){
         return view('about');
     });
     Route::get('/contact', function (){
         return view('pages.contact');
-    });
-});
+    })->middleware('age');
